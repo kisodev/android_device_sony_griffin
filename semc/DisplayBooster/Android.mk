94edeb3c1fa0cfb7502e367b83d9fd27f58885c5
moghn <1036664317@qq.com>
2022-05-23 16:13:45 +0800

    Revert "griffin: DisplayBooster to priv-app"
    This reverts commit 572b13a7c714ab94c4706f4741bf29d8d3763064.

--- semc/DisplayBooster/Android.mk
+++ semc/DisplayBooster/Android.mk
@@ -1,7 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := DisplayBooster
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
