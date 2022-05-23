572b13a7c714ab94c4706f4741bf29d8d3763064
moghn <1036664317@qq.com>
2022-05-23 14:12:46 +0800

    griffin: DisplayBooster to priv-app

--- semc/DisplayBooster/Android.mk
+++ semc/DisplayBooster/Android.mk
@@ -1,6 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := DisplayBooster
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 LOCAL_MODULE_CLASS := APPS
